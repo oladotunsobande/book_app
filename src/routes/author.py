2f2a8aa9b80e9e156c4d785409f3e186814e1628
Oladotun Sobande <oladotun@softcom.ng>
2020-09-22 12:47:05 +0100

    refactor: separation of concerns
    Separate controllers and endpoints.

--- src/routes/author.py
+++ src/routes/author.py
@@ -1,80 +1,15 @@
 from flask import Blueprint, request
-from kanpai import Kanpai
 
-from src.utils.responses import set_response
-from src.utils.exceptions import ValidationError
-import src.modules.author.service as authorService
+import src.modules.author.delivery.http as AuthorController
 
 author_routes = Blueprint("author_routes", __name__)
-
-def print_error(err):
-  print('Error: ',err)
 
 # Create new author
 @author_routes.route('/', methods=['POST'])
 def create_author():
-  try:
-    payload = request.get_json()
+  return AuthorController.create_author(request.get_json())
 
-    schema = Kanpai.Object({
-      'first_name': Kanpai.String(error="'first_name' must be a string")
-        .trim()
-        .required(error="'first_name' is required"),
-      'last_name': Kanpai.String(error="'last_name' must be a string")
-        .trim()
-        .required(error="'last_name' is required")
-    })
-
-    validation = schema.validate(payload)
-    if validation.get('error') != None:
-      error = list(validation.get('error').values())[0]
-      raise ValidationError(error)
-
-    return authorService.create_author(payload)
-  except ValidationError as err:
-    print_error(err)
-
-    return set_response(
-      status = 'BAD_REQUEST', 
-      success = False,
-      error = str(err)
-    )
-  except Exception as err:
-    print_error(err)
-
-    return set_response(
-      status = 'SERVER_ERROR', 
-      success = False,
-      error = 'An error occurred'
-    )    
-
+# Get single author
 @author_routes.route('/single/<int:author_id>', methods=['GET'])
 def get_author(author_id):
-  try:
-    schema = Kanpai.Object({
-      'author_id': Kanpai.Number(error="'author_id' must be a number")
-        .required(error="'author_id' is required")
-    })
-
-    validation = schema.validate({ "author_id": author_id })
-    if validation.get('error') != None:
-      error = list(validation.get('error').values())[0]
-      raise ValidationError(error)
-
-    return authorService.get_author(author_id)
-  except ValidationError as err:
-    print_error(err)
-
-    return set_response(
-      status = 'BAD_REQUEST', 
-      success = False,
-      error = str(err)
-    )
-  except Exception as err:
-    print_error(err)
-
-    return set_response(
-      status = 'SERVER_ERROR', 
-      success = False,
-      error = 'An error occurred'
-    )
+  return AuthorController.get_author(author_id)
